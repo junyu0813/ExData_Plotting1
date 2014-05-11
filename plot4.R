4bd2ee7a6d2e78aab0ca2916388c688b96fdb95e
junyu0813 <junyu0813@gmail.com>
2014-05-11 14:34:44 -0400

    plot4.R and its output

--- plot4.R
+++ plot4.R
@@ -1,5 +1,5 @@
 # In this program,  the data“Individual household electric power consumption Data Set” will be used. 
-# Plot3 is to plot Sub Meting 1-3 over a 2-day period in February, 2007.
+# Plot4 is to plot 4 figures in one plot.
 
 # set default directory
 setwd("C:/Users/Junyu/Documents/GitHub/ExData_Plotting1")
@@ -10,5 +10,28 @@ df <- read.table("household_power_consumption.txt", header=T, nrows=75000, sep="
 df <-df[df$Date=="1/2/2007" | df$Date=="2/2/2007",]
 x <- strptime(paste(df$Date, df$Time), format='%d/%m/%Y %H:%M:%S')
 
+# Prepare data
+#plot1
+gap<-as.vector(df[,3])
+gap <-as.numeric(as.vector(df[,3]))
 
+#plot3
+leg.tex <-c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 
+# Plot all figures
+png(filename = "plot4.png")
+par(mfrow=c(2, 2))
+
+#plot1
+hist(gap, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
+# plot2
+plot(x, df[,3], type="l", xlab="", ylab="Global Active Power (kilowatts)")
+#plot3
+plot(x, df[,7], type="l", xlab="", ylab="Energy sub metering")
+points(x, df[,8], type="l", col="red")
+points(x, df[,9], type="l", col="blue")
+legend("topright",legend = leg.tex, pch="_", text.col=c("black", "red", "blue"))
+#plot4
+plot(x, df[,4], type="l", xlab="", ylab="Global Active Power (kilowatts)")
+# Turn off
+dev.off()
